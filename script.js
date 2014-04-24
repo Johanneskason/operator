be14a0d933aa6812de0e8bcda5d9a3e6a3c26d92
Johannes Karlsson <johanneskason@gmail.com>
2014-04-24 15:25:32 +0200

    tredje commit

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
+var sum;
 var score = 0;
-var a = 45;
-var b = 10;
-var sum = 35;
+var a = Math.floor(Math.random()*10)+1;
+var b = Math.floor(Math.random()*10)+1;
+
+var operators=new Array("+","-","*","/");
+var random = Math.floor(Math.random()*3)+1;
+var op = operators[random];
+        switch( op ) {
+            case "+":
+                sum = a + b;
+                break;
+            case "-":
+                sum = a - b;
+                break;
+            case "*":
+                sum = a * b;            
+                break;
+            case "/":
+                sum = a / b;
+                break;
+            }
+
 document.getElementById("a").innerHTML = a;
 document.getElementById("b").innerHTML = b;
 document.getElementById("sum").innerHTML = sum;
 
+function calculate( operator ) {
+        var answer;
+        switch( operator ) {
+            case "+":
+                answer = a + b;
+                break;
+            case "-":
+                answer = a - b;
+                break;
+            case "*":
+                answer = a * b;            
+                break;
+            case "/":
+                answer = a / b;
+                break;
+            }
+        if(answer == sum){
+            document.getElementById("message").innerHTML = "RÄTT";
+            score += 1;
+        }
+        else{
+            document.getElementById("message").innerHTML = "FEL";
+            score = 0;
+            }
+        document.getElementById("score").innerHTML = "Din poäng är: " + score;   
+    }
+
 document.getElementById("addition").onclick = function() {
-    var answear = a + b;
-    
-    if(answear == sum){
-    document.getElementById("message").innerHTML = "RÄTT :)";
-    score = + 1;
-    }
-    else{
-        document.getElementById("message").innerHTML = "FEL :(";
-        return;
-    }
-}
-
+    calculate("+");
+};
 document.getElementById("subtraction").onclick = function() {
-    var answear = a - b;
-    
-    if(answear == sum){
-        document.getElementById("message").innerHTML = "RÄTT :)";
-        score = + 1;
-        document.getElementById("score").innerHTML = "Din poäng är: " + score;
-    }
-    else{
-        document.getElementById("message").innerHTML = "FEL :(";
-        return;
-    }
-}
-
+    calculate("-");
+};
 document.getElementById("multiplication").onclick = function() {
-    var answear = a * b;
-    
-    if(answear == sum){
-        document.getElementById("message").innerHTML = "RÄTT :)";
-        score = + 1;
-    }
-    else{
-        document.getElementById("message").innerHTML = "FEL :(";
-        return;
-    }
-}
+    calculate("*");
+};
 document.getElementById("division").onclick = function() {
-    var answear = a / b;
-    
-    if(answear == sum){
-        document.getElementById("message").innerHTML = "RÄTT :)";
-        score = + 1;
-    }
-    else{
-        document.getElementById("message").innerHTML = "FEL :(";
-        return;
-    }
-}
-document.getElementById("score").innerHTML = "Din poäng är: " + score;
+    calculate("/");
+};
+                                                        
 
 
 
