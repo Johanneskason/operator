3dafd94e9b986778eaf7916e927ed3786cbfb0ae
Johannes Karlsson <johanneskason@gmail.com>
2014-04-24 11:19:36 +0200

    min andra commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var score = 4;
+document.getElementById("a").innerHTML = 87234;
+document.getElementById("b").innerHTML = 1;
+document.getElementById("sum").innerHTML = 87233;
+document.getElementById("score").innerHTML = "Din poäng är: " + score;
+
+if(answear == correct){
+    document.getElementById("message").innerHTML = "RÄTT";
+}
+else{
+    document.getElementById("message").innerHTML = "FEL";
+}
