7c770ffa538605d9b456432e566597b506af39f4
Johannes Karlsson <johanneskason@gmail.com>
2014-04-24 11:55:00 +0200

    tredje commit

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
-var score = 4;
-document.getElementById("a").innerHTML = 87234;
-document.getElementById("b").innerHTML = 1;
-document.getElementById("sum").innerHTML = 87233;
+var score = 0;
+var a = 45;
+var b = 10;
+var sum = 35;
+document.getElementById("a").innerHTML = a;
+document.getElementById("b").innerHTML = b;
+document.getElementById("sum").innerHTML = sum;
+
+document.getElementById("addition").onclick = function() {
+    var answear = a + b;
+    
+    if(answear == sum){
+    document.getElementById("message").innerHTML = "RÄTT :)";
+    score = + 1;
+    }
+    else{
+        document.getElementById("message").innerHTML = "FEL :(";
+        return;
+    }
+}
+
+document.getElementById("subtraction").onclick = function() {
+    var answear = a - b;
+    
+    if(answear == sum){
+        document.getElementById("message").innerHTML = "RÄTT :)";
+        score = + 1;
+        document.getElementById("score").innerHTML = "Din poäng är: " + score;
+    }
+    else{
+        document.getElementById("message").innerHTML = "FEL :(";
+        return;
+    }
+}
+
+document.getElementById("multiplication").onclick = function() {
+    var answear = a * b;
+    
+    if(answear == sum){
+        document.getElementById("message").innerHTML = "RÄTT :)";
+        score = + 1;
+    }
+    else{
+        document.getElementById("message").innerHTML = "FEL :(";
+        return;
+    }
+}
+document.getElementById("division").onclick = function() {
+    var answear = a / b;
+    
+    if(answear == sum){
+        document.getElementById("message").innerHTML = "RÄTT :)";
+        score = + 1;
+    }
+    else{
+        document.getElementById("message").innerHTML = "FEL :(";
+        return;
+    }
+}
 document.getElementById("score").innerHTML = "Din poäng är: " + score;
 
-if(answear == correct){
-    document.getElementById("message").innerHTML = "RÄTT";
-}
-else{
-    document.getElementById("message").innerHTML = "FEL";
-}
+
+
